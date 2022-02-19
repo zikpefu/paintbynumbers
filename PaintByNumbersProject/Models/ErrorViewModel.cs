05ac960a2df258faecea12abd97a664328c022f1
Roger Smith Rivas <roger.smith.rivas@hotmail.com>
2022-02-18 19:59:48 -0500

    I just added the Asp.Net Core MVC backend with the identity model configured and did some small frontend changes.

---
+++ PaintByNumbersProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PaintByNumbersProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
