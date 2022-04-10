26359bb76987976fd2dc11e04ee4130b61e3d52c
William Harrison Tann <harrisontann@gmail.com>
2022-04-10 16:50:41 -0400

    Added new db context for adding images -crashes trying to add images

---
+++ PaintByNumbersProject/Models/ImageDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaintByNumbersProject.Models
+{
+    public class ImageDbContext:DbContext
+    {
+        public ImageDbContext(DbContextOptions<ImageDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<ImageModel> Images { get; set; }
+    }
+}
---
+++ PaintByNumbersProject/Models/ImageModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PaintByNumbersProject.Models
+{
+    public class ImageModel
+    {
+        [Key]
+        public int ImageId { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Image Name")]
+        public string ImageName { get; set; }
+
+        [NotMapped]
+        [DisplayName("Upload File")]
+        public IFormFile ImageFile { get; set; }
+    }
+}
