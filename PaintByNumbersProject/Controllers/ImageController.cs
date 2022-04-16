e4743e79aba49ab6846792a8c42c3f2bebe25e9b
Roger Smith Rivas <roger.smith.rivas@hotmail.com>
2022-04-16 16:47:31 -0400

    Help with the design of the design and I solve the error that prevents the image from loading

--- PaintByNumbersProject/Controllers/ImageController.cs
+++ PaintByNumbersProject/Controllers/ImageController.cs
@@ -23,7 +23,6 @@ namespace PaintByNumbersProject.Controllers
             return View(await _context.Images.ToListAsync());
         }
 
-        // GET: Image/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -41,91 +40,30 @@ namespace PaintByNumbersProject.Controllers
             return View(imageModel);
         }
 
-        // GET: Image/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Image/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("ImageId,Title,ImageFile")] ImageModel imageModel)
         {
-            if (ModelState.IsValid)
+            string wwwRootPath = _hostEnvironment.WebRootPath;
+            string fileName = Path.GetFileNameWithoutExtension(imageModel.ImageFile.FileName);
+            string extension = Path.GetExtension(imageModel.ImageFile.FileName);
+            imageModel.ImageName = fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+            //Save image to wwwroot/Images
+            string path = Path.Combine(wwwRootPath + "/Image/", fileName);
+            using (var fileStream = new FileStream(path, FileMode.Create))
             {
-                //Save image to wwwroot/Images
-                string wwwRootPath = _hostEnvironment.WebRootPath;
-                string fileName = Path.GetFileNameWithoutExtension(imageModel.ImageFile.FileName);
-                string extension = Path.GetExtension(imageModel.ImageFile.FileName);
-                imageModel.ImageName = fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
-                string path = Path.Combine(wwwRootPath + "/Image/", fileName);
-                using (var fileStream = new FileStream(path, FileMode.Create))
-                {
-                    await imageModel.ImageFile.CopyToAsync(fileStream);
-                }
-
-                _context.Add(imageModel);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                await imageModel.ImageFile.CopyToAsync(fileStream);
             }
-            return View(imageModel);
+            _context.Add(imageModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
 
-        // GET: Image/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var imageModel = await _context.Images.FindAsync(id);
-            if (imageModel == null)
-            {
-                return NotFound();
-            }
-            return View(imageModel);
-        }
-
-        // POST: Image/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ImageId,Title,ImageName")] ImageModel imageModel)
-        {
-            if (id != imageModel.ImageId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(imageModel);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ImageModelExists(imageModel.ImageId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(imageModel);
-        }
-
-        // GET: Image/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -143,7 +81,6 @@ namespace PaintByNumbersProject.Controllers
             return View(imageModel);
         }
 
-        // POST: Image/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
